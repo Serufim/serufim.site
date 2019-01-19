1aff4cb5903d48b945a88fb1d92cd53b648325dd
serufim <serufim97@yandex.ru>
2019-01-19 21:25:18 +0800

    admin command has been added

---
+++ app/Console/Commands/add_admin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class add_admin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'add:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'add Administrator';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+        $user = User::create([
+            'name' => '',
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+        $user->save();
+        $this->info('Done');
+    }
+}
